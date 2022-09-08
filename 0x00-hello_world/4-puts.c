b3a36716a96656ae9ba2c78e6e70bed61427554f
polanty <tijaniabiola1599@gmail.com>
2022-09-08 12:10:17 -0700

    Updated answers

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
  *main - Entry point
+ *Description:Write a C program that prints exactly \"Programming is like building a multilingual puzzle
  *Return: Always 0(success) 
- */
-int main (void) {
+*/
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
