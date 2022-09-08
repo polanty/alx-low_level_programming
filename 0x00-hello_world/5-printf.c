3e53972c976a3d018a2c56555981099e06e6ff99
polanty <tijaniabiola1599@gmail.com>
2022-09-08 14:29:48 -0700

    commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
- * Return : Always 0 (Success)
+ *
+ * Description:main- prints outs a string to stdout.
+ *
+ * Return : Always 0 (Success).
  */
 int main(void)
-{       /*Dscription: prints "with proper grammar, but the outcome is a piece of art," */
+{  
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
