06cc68fe2582efc3652a2474febc631f16624a74
polanty <tijaniabiola1599@gmail.com>
2022-09-09 15:42:17 -0700

    updated messages

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-
- * main - determine if number is positive or negative
-
+ *main - check if positive, zero or negative
  *
-
- * Description: program will assign a random number to the variable n each time
-
- * it is executed.
-
- *
-
- * Return: Always 0 (Success)
-
+ * Return: zero
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 	char ch;
 	char CH;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	     putchar(ch);
+	for (ch ='a'; ch <= 'z'; ch++)
+	    		 putchar(ch);
 	for (CH ='A'; CH <= 'Z'; CH++)
-	    putchar(CH);
+	   		 putchar(CH);
         putchar('\n'); 	
 
-	return (0);
+	return(0);
 }
