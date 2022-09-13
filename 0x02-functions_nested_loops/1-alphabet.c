605115d37cfd122045d479c7c318c2ea9ab3897c
polanty <tijaniabiola1599@gmail.com>
2022-09-13 06:38:39 -0700

    update message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * print_alphabet - print alphabet
  *
  * Return: void
  *
  */
-int main(void)
+void print_alphabet(void)
 {
 	char ch;
+
 	for(ch = 'a'; ch <= 'z'; ch++)
-	   {
-		   printf("%c\n", ch);
-           }
-        return(0);
+	{
+	
+     	   putchar(ch);
+
+        }
+	putchar('\n');
+
 }
