8109c992194d43f1ddea6e47a6c14d0c428707f4
polanty <tijaniabiola1599@gmail.com>
2022-09-13 03:20:32 -0700

    Initial commit for files function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ *
+ */
+int main(void)
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	   {
+		   printf("%c\n", ch);
+           }
+        return(0);
+}
